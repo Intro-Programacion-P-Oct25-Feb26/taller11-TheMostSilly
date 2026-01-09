57781784900ccb48005d972bd918cfe02f7d5763
TheMostSilly <geovannacastro058@gmail.com>
2026-01-08 19:33:33 -0500

    Ejercicio 06

--- ejercicios/Problemas/src/paquete06/Problema6.java
+++ ejercicios/Problemas/src/paquete06/Problema6.java
@@ -10,5 +10,38 @@ package paquete06;
  * @author reroes
  */
 public class Problema6 {
-    
+
+    public static void main(String[] args) {
+        int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+
+        double promedio = sacarPromedio(informacion);
+
+        double desviacion = sacarDesviacion(informacion, promedio);
+
+        System.out.printf("Media aritmética: %.2f\n", promedio);
+        System.out.printf("Desviación estándar: %.2f", desviacion);
+    }
+
+    public static double sacarPromedio(int[] a) {
+        int suma = 0;
+        double promedio;
+        for (int i = 0; i < a.length; i++) {
+            suma = suma + a[i];
+
+        }
+        promedio = suma / a.length;
+        return promedio;
+
+    }
+
+    public static double sacarDesviacion(int[] a, double b) {
+        double suma = 0;
+        for (int i = 0; i < a.length; i++) {
+            suma = suma + Math.pow(a[i] - b, 2);
+        }
+        double varianza = suma / a.length;
+        return Math.sqrt(varianza);
+
+    }
+
 }
